bf65332f3525f969cd2579e834001fa2d3fd5bb8
Truong Luong <namlongqn@gmail.com>
2017-07-28 23:57:32 +0700

    Udate use Unity
    Apply Dependence Injection for DatabaseMySQL

--- Blog/Controllers/VouxThemeController.cs
+++ Blog/Controllers/VouxThemeController.cs
@@ -3,6 +3,7 @@ using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Web.Mvc;
+using Blog.Areas.admin.ViewModels;
 using Blog.Infrastructure;
 using Blog.Models;
 using Dapper;
@@ -14,65 +15,28 @@ namespace Blog.Controllers
     {
         private const int PostsPerPage = 5;
 
-        private const string TypePost = "post";
+        private const string TypePost = "post";        
 
-        private readonly IDbConnection _db = new MySqlConnection(ConfigurationManager.ConnectionStrings["MySqlServerConnString"].ConnectionString);        
+        private readonly DatabaseMySql _data;
 
-        private IEnumerable<PostsRecentVoux> RecentVouexPosts(IEnumerable<long> ids, int take)
-        {            
-            var query =
-                "SELECT id, title, slug, created_at as created, updated_at as updated " +
-                "FROM posts " +
-                "WHERE type ='post' AND status = 'publish' AND id NOT IN (" + string.Join(",", ids) + ") " +
-                "ORDER BY id DESC " +
-                "LIMIT " + take;
-
-            return _db.Query<PostsRecentVoux>(query);
-        }
-
-        private IEnumerable<LabelVoux> GetTags()
+        public VouxThemeController(DatabaseMySql data)
         {
-            var query =
-                "SELECT t.id, t.name, t.slug, count(*) AS PostCount " +
-                "FROM term_posts tp, posts p, terms t " +
-                "WHERE tp.post_id = p.id " +
-                    "AND t.id = tp.term_id " +
-                    "AND t.taxonomy = 'tag' " +
-                    "AND p.type = 'post' " +
-                    "AND p.status = 'publish' " +
-                "GROUP BY t.id, t.name " +
-                "ORDER BY PostCount DESC" 
-                ;
-
-            return _db.Query<LabelVoux>(query);
+            _data = data;
         }
         public ActionResult Index(int page = 1)
         {
-
-            var queryCountPost =
-                "SELECT count(*) " +
-                "FROM posts " +
-                "WHERE type ='post' and status = 'publish'";//and created_at <= " + DateTime.Now;
-
-            var totalPostCount = _db.Query<int>(queryCountPost).Single();
-
-            var query =
-                "SELECT users.display_name as username, posts.id, title, posts.slug, excerpt, created_at as created, updated_at as updated, type, posts.status, user_id as userid " +
-                "FROM posts " +
-                "LEFT JOIN users ON (users.id = posts.user_id) " +
-                "WHERE type ='post' and posts.status = 'publish' " +
-                "ORDER BY id DESC " +
-                "LIMIT " + PostsPerPage + " OFFSET " + (page - 1) * PostsPerPage;
-                        
-            var posts = (List<PostVoux>)_db.Query<PostVoux>(query);
+            
+            var totalPostCount = _data.CountPostTypeAndStatus("post", "publish");
+            
+            var posts = _data.GetAllPostsPublish(page, PostsPerPage);
 
             var ids = posts
                 .Select(t => t.Id)
                 .ToArray();
 
-            ViewBag.RecentPosts = RecentVouexPosts(ids, 10);
+            ViewBag.RecentPosts = _data.RecentVouexPosts(ids, 10);
 
-            ViewBag.Tags = GetTags();            
+            ViewBag.Tags = _data.GetTags();            
                         
             return View(new PostsVouxIndex
             {
@@ -85,20 +49,14 @@ namespace Blog.Controllers
         /// <param name="slug">Slug of Post</param>
         /// <returns>Object PostsShow</returns>
         public ActionResult Show(string slug)
-        {
-            var slugPost = slug.Trim();
-
-            var query = "SELECT id, title, slug, excerpt, content, created_at as created, updated_at as updated " +
-                        "from posts p " +
-                        "WHERE status='publish' and type='post' and slug = '" + slugPost + "'";
-
-            var post = _db.Query<PostVoux>(query).SingleOrDefault();
+        {            
+            var post = _data.GetPost(slug);
 
             if (post == null) return HttpNotFound();            
 
-            ViewBag.RecentPosts = RecentVouexPosts(new []{post.Id}, 10);
+            ViewBag.RecentPosts = _data.RecentVouexPosts(new []{post.Id}, 10);
 
-            ViewBag.Tags = GetTags();
+            ViewBag.Tags = _data.GetTags();
 
             return View(post);
         }
@@ -109,46 +67,20 @@ namespace Blog.Controllers
         /// <param name="page">Trang hiện tại</param>
         /// <returns>Object PostsTag</returns>
         public ActionResult Tag(string slug, int page = 1)
-        {
-            var slugTag = slug.Trim();
-
-            var query = "select * from terms t where t.slug = '" + slugTag + "'";
-
-            var tag = _db.Query<LabelVoux>(query).SingleOrDefault();
+        {            
+            var tag = _data.GetCategory(slug);
 
             if (tag == null) return HttpNotFound();
+            
+            var totalPostCount = _data.CountPostPublishOfCategory(tag.Id);
+            
+            var posts = _data.GetPostsOfCategory(tag.Id, page, PostsPerPage);
 
-            var queryCount =
-                "SELECT count(*) " +
-                "FROM term_posts tp, terms t, posts p " +
-                "LEFT JOIN users ON(users.id = p.user_id) " +
-                "WHERE tp.post_id = p.id " +
-                    "AND t.id = tp.term_id " +
-                    "AND p.status = 'publish' " +
-                    "AND p.type = 'post' and t.id = " + tag.Id;
+            var ids = posts.Select(t => t.Id).ToArray();
 
-            var totalPostCount = _db.Query<int>(queryCount).SingleOrDefault();
+            ViewBag.RecentPosts = _data.RecentVouexPosts(ids, 10);
 
-            var queryMain =
-               "SELECT users.display_name as username, p.id, title, p.slug, excerpt, content, created_at as created, " +
-                       "updated_at as updated, type, p.status, user_id as userid " +
-               "FROM term_posts tp, terms t, posts p " +
-               "LEFT JOIN users ON(users.id = p.user_id) " +
-               "WHERE tp.post_id = p.id " +
-                   "AND t.id = tp.term_id " +
-                   "AND p.status = 'publish' " +
-                   "AND p.type = 'post' and t.id = " + tag.Id +
-               " ORDER BY id DESC " +
-               "LIMIT " + PostsPerPage + " OFFSET " + (page - 1) * PostsPerPage;
-
-            var posts = (List<PostVoux>)_db.Query<PostVoux>(queryMain);
-
-            var ids = posts
-                .Select(t => t.Id)
-                .ToArray();
-            ViewBag.RecentPosts = RecentVouexPosts(ids, 10);
-
-            ViewBag.Tags = GetTags();
+            ViewBag.Tags = _data.GetTags();
             
             return View(new PostsVouxTag
             {
--- Blog/Models/ModelVouxTheme.cs
+++ Blog/Models/ModelVouxTheme.cs
@@ -32,6 +32,137 @@ namespace Blog.Models
     public class DatabaseMySql
     {
         protected static IDbConnection Db => new MySqlConnection(ConfigurationManager.ConnectionStrings["MySqlServerConnString"].ConnectionString);
+                
+        public int CountPostTypeAndStatus(string type, string status)
+        {
+            var queryCountPost =
+               "SELECT COUNT(*) " +
+               "FROM posts " +
+               "WHERE type ='"+ type +"' AND status = '"+ status +"'";
+
+            return Db.Query<int>(queryCountPost).Single();
+        }
+
+        public List<PostVoux> GetAllPostsPublish(int page, int perPage)
+        {
+            var query =
+                "SELECT users.display_name as username, posts.id, title, posts.slug, excerpt, created_at as created, updated_at as updated, type, posts.status, user_id as userid " +
+                "FROM posts " +
+                "LEFT JOIN users ON (users.id = posts.user_id) " +
+                "WHERE type ='post' AND posts.status = 'publish' " +
+                "ORDER BY id DESC " +
+                "LIMIT " + perPage + " OFFSET " + (page - 1) * perPage;
+
+            return (List<PostVoux>)Db.Query<PostVoux>(query);
+        }
+        public PostVoux GetPost(string slugPost)
+        {
+            var query = "SELECT id, title, slug, excerpt, content, created_at as created, updated_at as updated " +
+                        "FROM posts p " +
+                        "WHERE status='publish' " +
+                          "AND type='post' " +
+                          "AND slug = '" + slugPost.Trim() + "'";
+
+            return Db.Query<PostVoux>(query).SingleOrDefault();            
+        }
+
+        public LabelVoux GetCategory(string slug)
+        {
+            var query = "SELECT * FROM terms t WHERE t.slug = '" + slug.Trim() + "'";
+
+            return Db.Query<LabelVoux>(query).SingleOrDefault();
+        }
+
+        public int CountPostPublishOfCategory(long id)
+        {
+
+            var queryCount =
+                "SELECT count(*) " +
+                "FROM term_posts tp, terms t, posts p " +
+                "LEFT JOIN users ON(users.id = p.user_id) " +
+                "WHERE tp.post_id = p.id " +
+                    "AND t.id = tp.term_id " +
+                    "AND p.status = 'publish' " +
+                    "AND p.type = 'post' and t.id = " + id;
+
+            return Db.Query<int>(queryCount).SingleOrDefault();
+        }
+
+        public List<PostVoux> GetPostsOfCategory(long idCategory,int page, int perPage)
+        {
+            var queryMain =
+              "SELECT users.display_name as username, p.id, title, p.slug, excerpt, content, created_at as created, " +
+                      "updated_at as updated, type, p.status, user_id as userid " +
+              "FROM term_posts tp, terms t, posts p " +
+              "LEFT JOIN users ON(users.id = p.user_id) " +
+              "WHERE tp.post_id = p.id " +
+                  "AND t.id = tp.term_id " +
+                  "AND p.status = 'publish' " +
+                  "AND p.type = 'post' and t.id = " + idCategory +
+              " ORDER BY id DESC " +
+              "LIMIT " + perPage + " OFFSET " + (page - 1) * perPage;
+            
+            return (List<PostVoux>)Db.Query<PostVoux>(queryMain);
+        }
+
+
+        public IEnumerable<PostsRecentVoux> RecentVouexPosts(IEnumerable<long> ids, int take)
+        {
+            var query =
+                "SELECT id, title, slug, created_at as created, updated_at as updated " +
+                "FROM posts " +
+                "WHERE type ='post' AND status = 'publish' AND id NOT IN (" + string.Join(",", ids) + ") " +
+                "ORDER BY id DESC " +
+                "LIMIT " + take;
+
+            return Db.Query<PostsRecentVoux>(query);
+        }
+
+        public IEnumerable<LabelVoux> GetTags()
+        {
+            var query =
+                "SELECT t.id, t.name, t.slug, count(*) AS PostCount " +
+                "FROM term_posts tp, posts p, terms t " +
+                "WHERE tp.post_id = p.id " +
+                    "AND t.id = tp.term_id " +
+                    "AND t.taxonomy = 'tag' " +
+                    "AND p.type = 'post' " +
+                    "AND p.status = 'publish' " +
+                "GROUP BY t.id, t.name " +
+                "ORDER BY PostCount DESC"
+                ;
+
+            return Db.Query<LabelVoux>(query);
+        }
+
+    }
+
+
+
+    public class PostsRecentVoux : DatabaseMySql
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Slug { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime? Updated { get; set; }
+        public string PostImage
+        {
+            get
+            {
+                var queryCountPost = "SELECT guid " +
+                                     "FROM posts " +
+                                     "WHERE posts.id IN (" +
+                                                        "SELECT meta_value " +
+                                                        "FROM postmeta m, posts p " +
+                                                        "WHERE p.id = m.post_id " +
+                                                            "AND meta_key = 'thumbnail_id' " +
+                                                            "AND post_id = " + Id + ")";
+
+                return Db.Query<string>(queryCountPost).FirstOrDefault();
+            }
+        }
+
     }
 
     public class PostVoux : PostsRecentVoux
@@ -74,35 +205,6 @@ namespace Blog.Models
         }
     }
 
-
-
-    public class PostsRecentVoux : DatabaseMySql
-    {
-
-        public long Id { get; set; }
-        public string Title { get; set; }
-        public string Slug { get; set; }
-        public DateTime Created { get; set; }
-        public DateTime? Updated { get; set; }        
-        public string PostImage
-        {
-            get
-            {
-                var queryCountPost = "SELECT guid " +
-                                     "FROM posts " +
-                                     "WHERE posts.id IN (" +
-                                                        "SELECT meta_value " +
-                                                        "FROM postmeta m, posts p " +
-                                                        "WHERE p.id = m.post_id " +
-                                                            "AND meta_key = 'thumbnail_id' " +
-                                                            "AND post_id = " + Id + ")";
-
-                return Db.Query<string>(queryCountPost).FirstOrDefault();
-            }            
-        }
-
-    }
-
     public class LabelVoux
     {
         
@@ -116,6 +218,7 @@ namespace Blog.Models
     {
         public PageData<PostVoux> Posts { get; set; }
     }
+
     public class PostsVouxTag
     {
         public LabelVoux Tag { get; set; }
